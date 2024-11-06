072b2041b4dad67eb92608e9672bad16e055fbeb
ettaoussiNouhaila <163061063+ettaoussiNouhaila@users.noreply.github.com>
2024-11-06 19:37:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu');
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if(themeBtn.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+
+};
+
+var swiper = new Swiper(".gallery-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction:false,
+    }
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+
+AOS.init({
+  duration:800,
+  delay:400
+});
